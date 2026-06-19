05cdc7655ad2166212193bc4d30166fd89cc0919
viper010221-lab <viper010221@gmail.com>
2026-06-19 07:33:36 +0300

    Update rulesMenu.js

--- src/commands/Utility/rulesMenu.js
+++ src/commands/Utility/rulesMenu.js
@@ -73,27 +73,27 @@ export default {
                             color: 'info',
                             description: 'Please review our community guidelines and official ban times below. Use common sense if it feels unfair, or you will get banned for it.',
                             fields: [
-                                { name: '⚖️ R1 No naked killing', value: '⏱️ **3h ban**', inline: true },
-                                { name: '⚖️ R2 No spawn camping', value: '⏱️ **5h ban**', inline: true },
-                                { name: '⚖️ R3 Lag machine', value: '⏱️ **3 days ban**', inline: true },
-                                { name: '⚖️ R4 Wither spawning', value: '⏱️ **1 day ban** (At Spawn)', inline: true },
-                                { name: '⚖️ R5 Chat rules', value: '⏱️ **2h ban**', inline: true },
-                                { name: '⚖️ R6 No Stasis Chambers', value: '⏱️ **3h ban** (In Combat)', inline: true },
-                                { name: '⚖️ R7 Pushing out of spawn', value: '⏱️ **5h ban**', inline: true },
-                                { name: '⚖️ R8 No elytra in combat', value: '⏱️ **4h ban**', inline: true },
-                                { name: '⚖️ R9 Noobie protection', value: '⏱️ **2h ban** (Abuse)', inline: true },
-                                { name: '⚖️ R10 Instant damage arrows', value: '⏱️ **3h ban**', inline: true },
-                                { name: '⚖️ R11 No crystals', value: '⏱️ **5h ban**', inline: true },
-                                { name: '⚖️ R12 String drop at spawn', value: '⏱️ **1h ban**', inline: true },
-                                { name: '⚖️ R13 No trident in combat', value: '⏱️ **4h ban**', inline: true },
-                                { name: '⚖️ R14 Toxicity', value: '⏱️ **3h ban**', inline: true },
-                                { name: '⚖️ R15 Excessive swearing', value: '⏱️ **1 day ban** (Chat Flood)', inline: true },
-                                { name: '⚖️ R16 English only', value: '⏱️ **2h ban**', inline: true },
-                                { name: '⚖️ R17 Advertising servers', value: '⏱️ **4h ban**', inline: true },
-                                { name: '⚖️ R18 Ticket spam', value: '⏱️ **7h ban**', inline: true },
-                                { name: '⚖️ R19 Incorrect team size', value: '⏱️ *Staff review*', inline: true },
-                                { name: '⚖️ R20 Duping/Cheating', value: '⏱️ **7 days ban**', inline: true },
-                                { name: '⚖️ R21 Staff Respect / Moderation', value: '⏱️ **6h ban** (Arguing with staff or about mod decisions)', inline: false },
+                                { name: ':scales: R1 No naked killing', value: '⏱️ **3h ban**', inline: true },
+                                { name: ':scales: R2 No spawn camping', value: '⏱️ **5h ban**', inline: true },
+                                { name: ':scales: R3 Lag machine', value: '⏱️ **3 days ban**', inline: true },
+                                { name: ':scales: R4 Wither spawning', value: '⏱️ **1 day ban** (At Spawn)', inline: true },
+                                { name: ':scales: R5 Chat rules', value: '⏱️ **2h ban**', inline: true },
+                                { name: ':scales: R6 No Stasis Chambers', value: '⏱️ **3h ban** (In Combat)', inline: true },
+                                { name: ':scales: R7 Pushing out of spawn', value: '⏱️ **5h ban**', inline: true },
+                                { name: ':scales: R8 No elytra in combat', value: '⏱️ **4h ban**', inline: true },
+                                { name: ':scales: R9 Noobie protection', value: '⏱️ **2h ban** (Abuse)', inline: true },
+                                { name: ':scales: R10 Instant damage arrows', value: '⏱️ **3h ban**', inline: true },
+                                { name: ':scales: R11 No crystals', value: '⏱️ **5h ban**', inline: true },
+                                { name: ':scales: R12 String drop at spawn', value: '⏱️ **1h ban**', inline: true },
+                                { name: ':scales: R13 No trident in combat', value: '⏱️ **4h ban**', inline: true },
+                                { name: ':scales: R14 Toxicity', value: '⏱️ **3h ban**', inline: true },
+                                { name: ':scales: R15 Excessive swearing', value: '⏱️ **1 day ban** (Chat Flood)', inline: true },
+                                { name: ':scales: R16 English only', value: '⏱️ **2h ban**', inline: true },
+                                { name: ':scales: R17 Advertising servers', value: '⏱️ **4h ban**', inline: true },
+                                { name: ':scales: R18 Ticket spam', value: '⏱️ **7h ban**', inline: true },
+                                { name: ':scales: R19 Incorrect team size', value: '⏱️ *Staff review*', inline: true },
+                                { name: ':scales: R20 Duping/Cheating', value: '⏱️ **7 days ban**', inline: true },
+                                { name: ':scales: R21 Staff Respect / Moderation', value: '⏱️ **6h ban** (Arguing with staff or about mod decisions)', inline: false },
                             ],
                             footer: "Reporting players requires evidence; please make sure to have evidence before making tickets. Please don't ask staff to check your tickets or ping them."
                         });
@@ -105,31 +105,31 @@ export default {
                         const bannedModsEmbed = createEmbed({
                             title: '🛡️ Banned Modifications & Client Restrictions',
                             color: 'error',
-                            description: 'Any modifications that grant an unfair advantage over vanilla players are strictly prohibited.\n\n⚖️ **Punishment = 4h-7h** (depends on what you used)',
+                            description: 'Any modifications that grant an unfair advantage over vanilla players are strictly prohibited.\n\n:scales: **Punishment = 4h-7h** (depends on what you used)',
                             fields: [
                                 { 
                                     name: '🚫 Hacked Clients & Movement Modifiers', 
-                                    value: 'Includes any mods that allow flight, speed hacking, freecam, or fast break.\n⚖️ **Punishment = 5 days ban**' 
+                                    value: 'Includes any mods that allow flight, speed hacking, freecam, or fast break.\n:scales: **Punishment = 5 days ban**' 
                                 },
                                 { 
                                     name: '👁️ X-Ray & Vision Enhancers', 
-                                    value: 'Mods that let you see through blocks, caves, or player names behind walls.\n⚖️ **Punishment = 4h-7h** (depends on what you used)' 
+                                    value: 'Mods that let you see through blocks, caves, or player names behind walls.\n:scales: **Punishment = 4h-7h** (depends on what you used)' 
                                 },
                                 { 
                                     name: '🤖 Automation & Macros', 
-                                    value: 'Auto-clicking/burst-clicking buttons, auto-sprint, auto-eating, and automated fishing or farming macros.\n⚖️ **Punishment = 7 hours ban**' 
+                                    value: 'Auto-clicking/burst-clicking buttons, auto-sprint, auto-eating, and automated fishing or farming macros.\n:scales: **Punishment = 7 hours ban**' 
                                 },
                                 { 
                                     name: '⚔️ PvP Assistance', 
-                                    value: 'Kill-aura, aim assist, reach extenders, auto-totem, and triggerbots.\n⚖️ **Punishment = 1 day ban**' 
+                                    value: 'Kill-aura, aim assist, reach extenders, auto-totem, and triggerbots.\n:scales: **Punishment = 1 day ban**' 
                                 },
                                 { 
                                     name: '⚠️ Malware & IP Stealers', 
-                                    value: 'Mods containing malicious code or scripts that attempt to access your computer’s IP or system information.\n⚖️ **Punishment = PERMANENT**' 
+                                    value: 'Mods containing malicious code or scripts that attempt to access your computer’s IP or system information.\n:scales: **Punishment = PERMANENT**' 
                                 },
                                 { 
                                     name: '🗺️ Unapproved Aesthetic Features', 
-                                    value: 'Certain minimap mods that indicate the location of nearby players or entities.\n⚖️ **Punishment = 6h ban**' 
+                                    value: 'Certain minimap mods that indicate the location of nearby players or entities.\n:scales: **Punishment = 6h ban**' 
                                 }
                             ],
                             footer: 'Use common sense if it feels unfair, or you will get banned for it.'
