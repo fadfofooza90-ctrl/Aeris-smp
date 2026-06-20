d141bdf3792eb1d5b50f68cb879c9fd6e94c3621
viper010221-lab <viper010221@gmail.com>
2026-06-21 02:21:03 +0300

    Update economy_dashboard.js

--- src/commands/Economy/modules/economy_dashboard.js
+++ src/commands/Economy/modules/economy_dashboard.js
@@ -257,7 +257,7 @@ async function handleAddCurrency(selectInteraction, rootInteraction, guild, clie
         .setStyle(TextInputStyle.Short)
         .setPlaceholder('wallet')
         .setMinLength(1)
-        .setMaxLength(5)
+        .setMaxLength(6)
         .setRequired(true);
 
     modal.addLabelComponents(userLabel);
@@ -374,7 +374,7 @@ async function handleRemoveCurrency(selectInteraction, rootInteraction, guild, c
         .setStyle(TextInputStyle.Short)
         .setPlaceholder('wallet')
         .setMinLength(1)
-        .setMaxLength(5)
+        .setMaxLength(6)
         .setRequired(true);
 
     modal.addLabelComponents(userLabel);
