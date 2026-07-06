c5b7464b5b35bed1718e70ae2d90f4d9bb252633
viper010221-lab <viper010221@gmail.com>
2026-07-06 20:10:01 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "FlowSMP community",
+        name: "NightfallSMP community",
         // Activity type number (0 = Playing).
         type: 3,
       },
